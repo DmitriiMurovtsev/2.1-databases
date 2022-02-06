43c8e3da4280d268bf9ebfd1adcc36ebefdfcd61
DmitriiMurovtsev <85881932+DmitriiMurovtsev@users.noreply.github.com>
2022-02-06 13:23:51 +0300

    add metod save in Phone

--- work_with_database/main/settings.py
+++ work_with_database/main/settings.py
@@ -51,9 +51,9 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_import_phones',
-        'USER': 'postgres',
-        'PASSWORD': 'Polop2020',
+        'NAME': 'demodb',
+        'USER': 'stacks',
+        'PASSWORD': 'stacks',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
@@ -8,3 +9,7 @@ class Phone(models.Model):
     image = models.CharField(max_length=200)
     release_date = models.CharField(max_length=50)
     lte_exists = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
